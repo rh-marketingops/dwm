64e1d488a7119306a63a53b788088ec4213ad978
colemanja91 <colemanja91@gmail.com>
2016-08-24 13:23:17 -0400

    started building tests for history

--- dwm/test/test_history.py
+++ dwm/test/test_history.py
@@ -1,53 +1,53 @@
-history_lookupAll_genericLookup_caught = {}
+history_genericLookup_caught = {}
 
-history_lookupAll_genericLookup_uncaught = {}
+history_genericLookup_uncaught = {}
 
-history_lookupAll_genericLookup_notChecked = {}
+history_genericLookup_notChecked = {}
 
-history_lookupAll_fieldSpecificLookup_caught = {}
+history_fieldSpecificLookup_caught = {}
 
-history_lookupAll_fieldSpecificLookup_uncaught = {}
+history_fieldSpecificLookup_uncaught = {}
 
-history_lookupAll_fieldSpecificLookup_notChecked = {}
+history_fieldSpecificLookup_notChecked = {}
 
-history_lookupAll_normLookup_caught = {}
+history_normLookup_caught = {}
 
-history_lookupAll_normLookup_uncaught = {}
+history_normLookup_uncaught = {}
 
-history_lookupAll_normLookup_notChecked = {}
+history_normLookup_notChecked = {}
 
-history_regexAll_genericRegex_caught = {}
+history_genericRegex_caught = {}
 
-history_regexAll_genericRegex_uncaught = {}
+history_genericRegex_uncaught = {}
 
-history_regexAll_genericRegex_notChecked = {}
+history_genericRegex_notChecked = {}
 
-history_regexAll_fieldSpecificRegex_caught = {}
+history_fieldSpecificRegex_caught = {}
 
-history_regexAll_fieldSpecificRegex_uncaught = {}
+history_fieldSpecificRegex_uncaught = {}
 
-history_regexAll_fieldSpecificRegex_notChecked = {}
+history_fieldSpecificRegex_notChecked = {}
 
-history_regexAll_normRegex_caught = {}
+history_normRegex_caught = {}
 
-history_regexAll_normRegex_uncaught = {}
+history_normRegex_uncaught = {}
 
-history_regexAll_normRegex_notChecked = {}
+history_normRegex_notChecked = {}
 
-history_deriveAll_deriveValue_caught = {}
+history_deriveValue_caught = {}
 
-history_deriveAll_deriveValue_overwriteFalse = {}
+history_deriveValue_overwriteFalse = {}
 
-history_deriveAll_deriveValue_uncaught = {}
+history_deriveValue_uncaught = {}
 
-history_deriveAll_deriveValue_notChecked = {}
+history_deriveValue_notChecked = {}
 
-history_deriveAll_copyValue = {}
+history_copyValue = {}
 
-history_deriveAll_deriveRegex_caught = {}
+history_deriveRegex_caught = {}
 
-history_deriveAll_deriveRegex_uncaught = {}
+history_deriveRegex_uncaught = {}
 
-history_deriveAll_deriveRegex_notChecked = {}
+history_deriveRegex_notChecked = {}
 
-history_deriveAll_deriveRegex_overwriteFalse = {}
+history_deriveRegex_overwriteFalse = {}
