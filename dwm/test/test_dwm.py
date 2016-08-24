52151809abed94e02cd71f536d09bf2cd23cdfcb
colemanja91 <colemanja91@gmail.com>
2016-08-24 14:38:52 -0400

    finished tests for history

--- dwm/test/test_dwm.py
+++ dwm/test/test_dwm.py
@@ -235,137 +235,160 @@ def test_history_fieldSpecificLookup_caught():
     hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
     assert hist['field1']['fieldSpecificLookup']['from'] == 'badvalue'
 
-# def test_history_fieldSpecificLookup_uncaught():
-#
-#     dataOut = dwmAll(data = test_records.record_lookupAll_fieldSpecificLookup_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_fieldSpecificLookup')
-#     assert dataOut[0]['field1'] != ''
-#
-# def test_history_fieldSpecificLookup_notChecked():
-#
-#     dataOut = dwmAll(data = test_records.record_lookupAll_fieldSpecificLookup_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_fieldSpecificLookup')
-#     assert dataOut[0]['field2'] != ''
-#
-# # normLookup
-#
-# def test_history_normLookup_caught():
-#
-#     # do stuff for testing
-#     dataOut = dwmAll(data = test_records.record_lookupAll_normLookup_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_normLookup')
-#     assert dataOut[0]['field1'] == 'goodvalue'
-#
-# def test_history_normLookup_uncaught():
-#
-#     dataOut = dwmAll(data = test_records.record_lookupAll_normLookup_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_normLookup')
-#     assert dataOut[0]['field1'] != 'goodvalue'
-#
-# def test_history_normLookup_notChecked():
-#
-#     dataOut = dwmAll(data = test_records.record_lookupAll_normLookup_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_normLookup')
-#     assert dataOut[0]['field2'] != 'goodvalue'
-#
-# # genericRegex
-#
-# def test_history_genericRegex_caught():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_genericRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_genericRegex')
-#     assert dataOut[0]['field1'] == ''
-#
-# def test_history_genericRegex_uncaught():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_genericRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_genericRegex')
-#     assert dataOut[0]['field1'] != ''
-#
-# def test_history_genericRegex_notChecked():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_genericRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_genericRegex')
-#     assert dataOut[0]['field2'] != ''
-#
-# # fieldSpecificRegex
-#
-# def test_history_fieldSpecificRegex_caught():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_fieldSpecificRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_fieldSpecificRegex')
-#     assert dataOut[0]['field1'] == ''
-#
-# def test_history_fieldSpecificRegex_uncaught():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_fieldSpecificRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_fieldSpecificRegex')
-#     assert dataOut[0]['field1'] != ''
-#
-# def test_history_fieldSpecificRegex_notChecked():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_fieldSpecificRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_fieldSpecificRegex')
-#     assert dataOut[0]['field2'] != ''
-#
-# # normRegex
-#
-# def test_history_normRegex_caught():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_normRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_normRegex')
-#     assert dataOut[0]['field1'] == 'goodvalue'
-#
-# def test_history_normRegex_uncaught():
-#
-#     # do stuff for testing
-#     dataOut = dwmAll(data = test_records.record_regexAll_normRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_normRegex')
-#     assert dataOut[0]['field1'] != 'goodvalue'
-#
-# def test_history_normRegex_notChecked():
-#
-#     dataOut = dwmAll(data = test_records.record_regexAll_normRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_normRegex')
-#     assert dataOut[0]['field2'] != 'goodvalue'
-#
-# # deriveValue
-#
-# def test_history_deriveValue_caught():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveValue_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue')
-#     assert dataOut[0]['field1'] == 'newvalue'
-#
-# def test_history_deriveValue_uncaught():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveValue_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue')
-#     assert dataOut[0]['field1'] != 'newvalue'
-#
-# def test_history_deriveValue_notChecked():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveValue_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue')
-#     assert dataOut[0]['field3'] == ''
-#
-# def test_history_deriveValue_overwriteFalse():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveValue_overwriteFalse, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue_overwriteFalse')
-#     assert dataOut[0]['field1'] == 'oldvalue'
-#
-# # copyValue
-#
-# def test_history_copyValue():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_copyValue, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_copyValue')
-#     assert dataOut[0]['field1'] == 'newvalue'
-#
-# # deriveRegex
-#
-# def test_history_deriveRegex_caught():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex')
-#     assert dataOut[0]['field1'] == 'newvalue'
-#
-# def test_history_deriveRegex_uncaught():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex')
-#     assert dataOut[0]['field1'] != 'newvalue'
-#
-# def test_history_deriveRegex_notChecked():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex')
-#     assert dataOut[0]['field3'] == ''
-#
-# def test_history_deriveRegex_overwriteFalse():
-#
-#     dataOut = dwmAll(data = test_records.record_deriveAll_deriveRegex_overwriteFalse, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex_overwriteFalse')
-#     assert dataOut[0]['field1'] == 'oldvalue'
-#
+def test_history_fieldSpecificLookup_uncaught():
+
+    dataOut = dwmAll(data = test_records.history_fieldSpecificLookup_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_fieldSpecificLookup')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+def test_history_fieldSpecificLookup_notChecked():
+
+    dataOut = dwmAll(data = test_records.history_fieldSpecificLookup_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_fieldSpecificLookup')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field2' not in hist.keys()
+
+# normLookup
+
+def test_history_normLookup_caught():
+
+    # do stuff for testing
+    dataOut = dwmAll(data = test_records.history_normLookup_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_normLookup')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert hist['field1']['normLookup']['from'] == 'badvalue'
+
+def test_history_normLookup_uncaught():
+
+    dataOut = dwmAll(data = test_records.history_normLookup_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_normLookup')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+def test_history_normLookup_notChecked():
+
+    dataOut = dwmAll(data = test_records.history_normLookup_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_lookupAll_normLookup')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field2' not in hist.keys()
+
+# genericRegex
+
+def test_history_genericRegex_caught():
+
+    dataOut = dwmAll(data = test_records.history_genericRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_genericRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert hist['field1']['genericRegex']['from'] == 'badvalue'
+
+def test_history_genericRegex_uncaught():
+
+    dataOut = dwmAll(data = test_records.history_genericRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_genericRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+def test_history_genericRegex_notChecked():
+
+    dataOut = dwmAll(data = test_records.history_genericRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_genericRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field2' not in hist.keys()
+
+# fieldSpecificRegex
+
+def test_history_fieldSpecificRegex_caught():
+
+    dataOut = dwmAll(data = test_records.history_fieldSpecificRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_fieldSpecificRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert hist['field1']['fieldSpecificRegex']['from'] == 'badvalue'
+
+def test_history_fieldSpecificRegex_uncaught():
+
+    dataOut = dwmAll(data = test_records.record_regexAll_fieldSpecificRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_fieldSpecificRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+def test_history_fieldSpecificRegex_notChecked():
+
+    dataOut = dwmAll(data = test_records.history_fieldSpecificRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_fieldSpecificRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field2' not in hist.keys()
+
+# normRegex
+
+def test_history_normRegex_caught():
+
+    dataOut = dwmAll(data = test_records.history_normRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_normRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert hist['field1']['normRegex']['from'] == 'badvalue'
+
+def test_history_normRegex_uncaught():
+
+    # do stuff for testing
+    dataOut = dwmAll(data = test_records.history_normRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_normRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+def test_history_normRegex_notChecked():
+
+    dataOut = dwmAll(data = test_records.history_normRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_regexAll_normRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field2' not in hist.keys()
+
+# deriveValue
+
+def test_history_deriveValue_caught():
+
+    dataOut = dwmAll(data = test_records.history_deriveValue_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert hist['field1']['deriveValue']['from'] == ''
+
+def test_history_deriveValue_uncaught():
+
+    dataOut = dwmAll(data = test_records.history_deriveValue_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+def test_history_deriveValue_notChecked():
+
+    dataOut = dwmAll(data = test_records.history_deriveValue_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field3' not in hist.keys()
+
+def test_history_deriveValue_overwriteFalse():
+
+    dataOut = dwmAll(data = test_records.history_deriveValue_overwriteFalse, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveValue_overwriteFalse')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+# copyValue
+
+def test_history_copyValue():
+
+    dataOut = dwmAll(data = test_records.history_copyValue, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_copyValue')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert hist['field1']['copyValue']['from'] == ''
+
+# deriveRegex
+
+def test_history_deriveRegex_caught():
+
+    dataOut = dwmAll(data = test_records.history_deriveRegex_caught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert hist['field1']['deriveRegex']['from'] == ''
+
+def test_history_deriveRegex_uncaught():
+
+    dataOut = dwmAll(data = test_records.history_deriveRegex_uncaught, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
+def test_history_deriveRegex_notChecked():
+
+    dataOut = dwmAll(data = test_records.history_deriveRegex_notChecked, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field3' not in hist.keys()
+
+def test_history_deriveRegex_overwriteFalse():
+
+    dataOut = dwmAll(data = test_records.history_deriveRegex_overwriteFalse, mongoDb = db, mongoConfig=mongoConfig, configName='test_deriveAll_deriveRegex_overwriteFalse')
+    hist = db.contactHistory.find_one({"_id": dataOut[0]['historyId']})
+    assert 'field1' not in hist.keys()
+
 
 #########################################
 ## Test options around history
