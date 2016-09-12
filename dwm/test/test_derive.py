562ccc5731768eea5a41190a3d8bd9e478242ac7
colemanja91 <colemanja91@gmail.com>
2016-09-12 14:51:17 -0400

    remove old files from test directory

--- dwm/test/test_derive.py
+++
@@ -1,6 +0,0 @@
-derive = [
-    {"type": "deriveValue", "fieldName": "field1", "lookupVals": {"field2": "FINDTHIS"}, "value": "newvalue"},
-    {"type": "deriveRegex", "fieldName": "field1", "deriveFieldName": "field2", "pattern": "^findthis$", "replace": "newvalue"},
-    {"type": "deriveValue", "fieldName": "field1", "lookupVals": {"field4": "NOFINDTHIS"}, "value": "incorrectvalue"},
-    {"type": "deriveValue", "fieldName": "field1", "lookupVals": {"field3": "FINDTHIS"}, "value": "correctvalue"}
-]
--- dwm/test/test_lookup.py
+++
@@ -1,5 +0,0 @@
-lookup = [
-    {"type": "genericLookup", "find": "BADVALUE"},
-    {"type": "fieldSpecificLookup", "fieldName": "field1", "find": "BADVALUE"},
-    {"type": "normLookup", "fieldName": "field1", "find": "BADVALUE", "replace": "goodvalue"}
-]
