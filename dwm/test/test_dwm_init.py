7193e34999fb9e8ebb873353f3343f44fd7ed7aa
colemanja91 <colemanja91@gmail.com>
2017-04-11 10:22:29 -0400

    test initialization defaults and values

--- dwm/test/test_dwm_init.py
+++ dwm/test/test_dwm_init.py
@@ -5,9 +5,33 @@ from mock import patch
 
 from dwm import Dwm
 
+# Setup mongomock db
+
+DB = mongomock.MongoClient().db
+
 # Initialization tests
 
 def test_dwm_initialize():
     """ Test that Dwm class initializes with proper object type """
-    dwm = Dwm()
+    dwm = Dwm(name='test', mongo=DB)
     assert isinstance(dwm, Dwm)
+
+def test_dwm_init_name():
+    """ test Dwm class sets name variable """
+    dwm = Dwm(name='test', mongo=DB)
+    assert dwm.name == 'test'
+
+def test_dwm_init_mongo():
+    """ test Dwm class sets MongoDB client """
+    dwm = Dwm(name='test', mongo=DB)
+    assert dwm.mongo == DB
+
+def test_dwm_init_default_fields():
+    """ test Dwm class initializes with default fields empty """
+    dwm = Dwm(name='test', mongo=DB)
+    assert dwm.fields == []
+
+def test_dwm_init_default_udf():
+    """ test Dwm class initializes with default fields empty """
+    dwm = Dwm(name='test', mongo=DB)
+    assert dwm.udfs == []
