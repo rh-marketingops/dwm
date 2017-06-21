d55604ad688cbccefd494664d7e7101b186432fa
pramod toraskar <ptoraska@redhat.com>
2017-06-21 17:41:08 +0530

    add tests for DWM class drive method cover deriveIncludes case

--- dwm/test/test_derive_includes.py
+++ dwm/test/test_derive_includes.py
@@ -1,81 +1,146 @@
-""" test generic validation lookup function """
+""" test derive includes case of derive function"""
 
 import mongomock
-#from mock import patch
-#from nose.tools import raises
-from .test_deriveValue import deriveValue
 
 from dwm import Dwm
+from .test_deriveIncludes import deriveIncludes
+
 
 # Setup mongomock db
-
 DB = mongomock.MongoClient().db
 
-DB.deriveValue.insert_many({"fieldName": "vertical", "deriveFieldName": "emailAddress", "includes": "", "excludes": "", "begins": "", "ends": ".GOV","replace": "Government"})
+DB.deriveIncludes.insert_many(deriveIncludes)
 
 # Setup Dwm instance
-
 FIELDS = {
-    'vertical': {
-        'lookup': [],
-        'derive': [
+    "field1": {
+        "lookup": [],
+        "derive": [
             {
-                'type': 'deriveIncludes',
-                'fieldSet': ['emailAddress'],
-                'options': ['overwrite']
-            },
-            {
-                'type': 'copyValue',
-                'fieldSet': ['industry'],
-                'options': ['overwrite']
+                "type": "deriveIncludes",
+                "fieldSet": ["field2"],
+                "options": ["overwrite"]
             }
         ]
-        }
+    }
 }
+
 
 DWM = Dwm(name='test', mongo=DB, fields=FIELDS)
 
+FIELDS_OVERWRITE_FALSE = {
+    "field1": {
+        "lookup": [],
+        "derive": [
+            {
+                "type": "deriveIncludes",
+                "fieldSet": ["field2"],
+                "options": []
+            }
+        ]
+    }
+}
+
+DWM_OVERWRITE_FALSE_OBJ = Dwm(name='test', mongo=DB,
+                              fields=FIELDS_OVERWRITE_FALSE)
+
+FIELDS_BLANK_IF_NO_MATCH = {
+    "field1": {
+        "lookup": [],
+        "derive": [
+            {
+                "type": "deriveIncludes",
+                "fieldSet": ["field2"],
+                "options": ["overwrite", "blankIfNoMatch"]
+            }
+        ]
+    }
+}
+
+DWM_BLANK_IF_NO_MATCH_OBJ = Dwm(name='test', mongo=DB,
+                                fields=FIELDS_BLANK_IF_NO_MATCH)
+
 
 # Let the testing begin
-def test_deriveAll_deriveIncludes_included_caught():
-
-    rec = {"emailAddress": "test@test.com", "field1": "", "field2": "findgoodinvaluejunk"}
-    rec_out, _ = DWM._derive(rec, {})
-    print(rec_out)
-    assert rec_out == {'field1' == 'goodvalue'}
-
-'''
-def test_dwm_derive_deriveValue_caught():
-    """ Ensure generic lookup occurs """
-    rec = {"emailAddress": "test@test.com", "field1": "", "field2": "findthis"}
-    rec_out, _ = DWM._derive(rec, {}) #pylint: disable=W0212
-    print(rec_out)
-    assert rec_out == {'field1': 'newvalue'}
-
-def test_dwm_vg_lup_good():
-    """ Ensure good value not cleared """
-    rec = {'field1': 'GOODVALUE'}
-    rec_out, _ = DWM._val_g_lookup(rec, {}) #pylint: disable=W0212
-    assert rec_out == rec
+def test_derive_includes_included_caught():
+    """ Ensure derive includes caught """
+    rec = {"emailAddress": "test@test.com", "field1": "",
+           "field2": "findgoodinvaluejunk"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] == 'goodvalue'
 
 
-def test_dwm_vg_lup_badcln():
-    """ Ensure basic lookup occurs and cleans value before """
-    rec = {'field1': '  badvalue\r\n  '}
-    rec_out, _ = DWM._val_g_lookup(rec, {}) #pylint: disable=W0212
-    assert rec_out == {'field1': ''}
+def test_derive_includes_included_uncaught():
+    """ Ensure derive includes uncaught """
+    rec = {"emailAddress": "test@test.com", "field1": "", "field2": "nothere"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] != 'goodvalue'
 
 
-def test_dwm_vg_lup_badmulti():
-    """ Ensure lookup occurs on every field in config """
-    rec = {'field1': 'BADVALUE', 'field2': 'BADVALUE'}
-    rec_out, _ = DWM._val_g_lookup(rec, {}) #pylint: disable=W0212
-    assert rec_out == {'field1': '', 'field2': ''}
+def test_derive_includes_excluded_caught():
+    """ Ensure derive includes excluded caught """
+    rec = {"emailAddress": "test@test.com", "field1": "",
+           "field2": "findgoodinvaluejunk butstuffnobad"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] != 'goodvalue'
 
 
-def test_dwm_vg_lup_leave():
-    """ Ensure lookup does not occur on field not in config """
-    rec = {'field1': 'BADVALUE', 'field3': 'BADVALUE'}
-    rec_out, _ = DWM._val_g_lookup(rec, {}) #pylint: disable=W0212
-    assert rec_out == {'field1': '', 'field3': 'BADVALUE'}
-'''
+def test_derive_includes_excluded_uncaught():
+    """ Ensure derive includes excluded uncaught """
+    rec = {"emailAddress": "test@test.com", "field1": "",
+           "field2": "findgoodinvaluejunk uncaught"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] == 'goodvalue'
+
+
+def test_derive_includes_begins_caught():
+    """ Ensure derive includes begins caught """
+    rec = {"emailAddress": "test@test.com", "field1": "", "field2": "abcdefg"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] == 'goodvalue'
+
+
+def test_derive_includes_begins_uncaught():
+    """ Ensure derive includes begins uncaught """
+    rec = {"emailAddress": "test@test.com", "field1": "", "field2": "hijklmnop"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] != 'goodvalue'
+
+
+def test_derive_includes_ends_caught():
+    """ Ensure derive includes ends caught """
+    rec = {"emailAddress": "test@test.com", "field1": "",
+           "field2": "qrstuvwxyz"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] == 'goodvalue'
+
+
+def test_derive_includes_ends_uncaught():
+    """ Ensure derive includes ends uncaught """
+    rec = {"emailAddress": "test@test.com", "field1": "", "field2": "notalpha"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] != 'goodvalue'
+
+
+def test_derive_includes_not_checked():
+    """ Ensure derive includes not checked """
+    rec = {"emailAddress": "test@test.com", "field2": "",
+           "field3": "doesnotmatter"}
+    rec_out, _ = DWM._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field2'] != 'goodvalue'
+
+
+def test_derive_includes_over_write_false():
+    """ Ensure derive includes over write false"""
+    rec = {"emailAddress": "test@test.com", "field1": "oldvalue",
+           "field2": "findgoodinvaluejunk"}
+    rec_out, _ = DWM_OVERWRITE_FALSE_OBJ._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] == 'oldvalue'
+
+
+def test_derive_includes_blank_if_no_match():
+    """ Ensure derive includes blank if no match"""
+    rec = {"emailAddress": "test@test.com", "field1": "oldvalue",
+           "field2": "nothere"}
+    rec_out, _ = DWM_BLANK_IF_NO_MATCH_OBJ._derive(rec, {})  # pylint: disable=W0212
+    assert rec_out['field1'] == ''
